d666c9e31f0dcdd9ca7a10a285aceb0ac91db1de
ruqiya <ruqaiyaahmed33@icloud.com>
2023-07-16 02:38:53 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main-Entry point
+ * main - Entry point
  *
- * return: Always 0(Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}
+
+
